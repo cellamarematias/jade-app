411ffaf02bbae844403d45b2405475b1fc5e3464
Matías Cellamare <88092713+cellamarematias@users.noreply.github.com>
2022-08-26 11:47:15 -0300

    Firts settings

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Screens here
+import Login from './screens/Loogin';
+
+const Stack = createNativeStackNavigator();
+
+// el orden de las pantallas se corresponden al orden en el que se muestran
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyStack/>
+    </NavigationContainer>
   );
 }
 
