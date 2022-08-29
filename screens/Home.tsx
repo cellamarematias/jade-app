aa5775c07402dd02af3c18f5b43ca279f3f39578
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-08-29 15:18:06 -0300

    setup fix

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { View, Text } from 'react-native';
-import NavBar from "../components/navbar/BottomTab";
-
+import { View, Text } from "react-native";
+// import NavBar from "../components/navbar/BottomTab";
 
 const Home = () => {
-	return (
-		<View>
-			<Text>Home</Text>
-		<NavBar />
-		</View>
-	)
-}
+  return (
+    <View>
+      <Text>Home</Text>
+      {/* <NavBar /> */}
+    </View>
+  );
+};
 
 export default Home;
