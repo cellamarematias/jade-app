db18a5c522c5654683ccb0e9e6febe1009e3855d
nicolas costanza <nicolasantoniocostanza@gmail.com>
2022-09-08 10:01:53 -0300

    asd

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,8 +4,6 @@ import Navigation from "./Navigation/LoggedNavigation";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Home = () => {
-  // si queremos reloguer, descomentar la linea de abajo y recargar
-  // AsyncStorage.setItem("token", "");
   return <Text>Home screen</Text>;
 };
 
